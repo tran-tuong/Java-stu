fa04ff70cca035a6342c572649ede6e0282dba25
Tuong Tran <tranvantuong2k3@gmail.com>
2025-03-05 01:41:29 +0700

    initial project

---
+++ src/main/java/com/samsung/enrollmentManagement/EnrollmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.samsung.enrollmentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsung/enrollmentManagement/EnrollmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsung.enrollmentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
